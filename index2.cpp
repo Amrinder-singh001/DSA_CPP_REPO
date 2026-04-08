1449c190e7eb885af1b1e089d94dd1292fc8d056
Amardeep singh <Amardeep@Macs-MacBook-Pro-767.local>
2026-04-08 14:29:38 +0530

    New index2.cpp file

---
+++ index2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+     cout << "New 2 index.cpp file" << endl;
+    
+    return 0;
+}
